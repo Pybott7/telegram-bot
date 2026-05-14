020d69abfde7f83f4ab6de95d4395a1497b439c4
Pybott7 <mahekcollab@gmail.com>
2026-05-14 19:42:21 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+```python id="jlwm5l"
+from os import getenv
+
+API_ID = int(getenv("API_ID"))
+
+API_HASH = getenv("API_HASH")
+
+BOT_TOKEN = getenv("BOT_TOKEN")
+
+ADMIN_ID = int(getenv("ADMIN_ID"))
+
+STORAGE_CHANNEL_ID = int(getenv("STORAGE_CHANNEL_ID"))
+```
